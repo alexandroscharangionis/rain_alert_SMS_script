d9ec9ffd2ff139149dd3a38e96b3b7970073938b
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-28 16:12:18 +0200

    excluded some unwanted data from API via input parameters

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ api_key = "69f04e4613056b159c2761a9d9e664d2"
 weather_params = {
     "lat": 44.426765,
     "lon": 26.102537,
-    "appid": api_key
-}
+    "appid": api_key,
+    "exclude": "current,minutely,daily"}
 response = requests.get(OMW_endpoint, params=weather_params)
-info = response.json()
+response.raise_for_status()
+
+weather_data = response.json()
+print(weather_data)
