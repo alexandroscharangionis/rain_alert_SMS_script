b3602a5e68de4b302a1b2129c3eeccf0ba5061c4
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-28 16:06:33 +0200

    Open Weather Map API connection

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+OMW_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+# api_key = "64a58c3612db289bb4f64ab7d140d7b9"
+api_key = "69f04e4613056b159c2761a9d9e664d2"
+
+weather_params = {
+    "lat": 44.426765,
+    "lon": 26.102537,
+    "appid": api_key
+}
+response = requests.get(OMW_endpoint, params=weather_params)
+info = response.json()
