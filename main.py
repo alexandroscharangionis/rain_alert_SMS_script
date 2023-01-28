10918b1d9967d0ee8d6c786afa67bfe2569f2607
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-28 16:26:15 +0200

    12 hour wether code checking logic

--- main.py
+++ main.py
@@ -13,4 +13,10 @@ response = requests.get(OMW_endpoint, params=weather_params)
 response.raise_for_status()
 
 weather_data = response.json()
-print(weather_data)
+weather_data_12_hours = weather_data["hourly"][:12]
+will_rain = False
+
+for hour_data in weather_data_12_hours:
+    weather_condition_code = hour_data["weather"][0]["id"]
+    if int(weather_condition_code) < 700:
+        will_rain = True
