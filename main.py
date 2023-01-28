777527e65c3d52c7a230a419cf377173d8060c86
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-28 17:08:26 +0200

    Twilio authentication setup

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
+from twilio.rest import Client
 
+# Authentication info from Twilio:
+ACCOUNT_SID = "AC49339c118b248f6c31e783077c669551"
+AUTH_TOKEN = "ca62675bb82f970e875baec4373df27f"
+
+# Open Weather Map API info:
 OMW_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-# api_key = "64a58c3612db289bb4f64ab7d140d7b9"
 api_key = "69f04e4613056b159c2761a9d9e664d2"
 
 weather_params = {
